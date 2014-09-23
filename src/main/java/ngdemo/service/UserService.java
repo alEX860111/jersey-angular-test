e1a68c3b999e2c80634b28d77071d0fa25ed5b08
alEX860111 <alex86011119@gmail.com>
2014-09-23 18:26:51 +0200

    got it running

---
+++ src/main/java/ngdemo/service/UserService.java
@@ -0,0 +1,14 @@
+package ngdemo.service;
+
+import ngdemo.domain.User;
+
+public class UserService {
+
+    public User getDefaultUser() {
+        User user = new User();
+        user.setFirstName("JonFromREST");
+        user.setLastName("DoeFromREST");
+        return user;
+    }
+
+}
