e1aa2d4705c576e2bf85c1468a545ed8166497ac
alEX860111 <alex86011119@gmail.com>
2014-10-03 15:27:03 +0200

    added e2e test

--- src/main/webapp/app/gruntfile.js
+++ src/main/webapp/app/gruntfile.js
@@ -7,9 +7,26 @@ module.exports = function(grunt) {
             unit: {
                 configFile: 'test/karma.conf.js'
             }
+        },
+        protractor: {
+            options: {
+                  configFile: "test/e2e/conf.js", //your protractor config file
+                  keepAlive: false, // If false, the grunt process stops when the test fails.
+                  noColor: false, // If true, protractor will not use colors in its output.
+                  args: {
+                      // Arguments passed to the command
+                  }
+              },
+            chrome: {
+                options: {
+                      args: {
+                          browser: "chrome"
+                      }
+                  }
+            }
         }
     });
  
     grunt.loadNpmTasks('grunt-karma');
-    grunt.registerTask('default', ['karma']);
+    grunt.loadNpmTasks('grunt-protractor-runner');
 };
