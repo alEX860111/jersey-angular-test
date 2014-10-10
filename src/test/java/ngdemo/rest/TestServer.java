3e865ea36be5e7338deda669893dfe824ecc5ee3
alEX860111 <alex86011119@gmail.com>
2014-10-10 19:20:02 +0200

    using only one jetty version for testing

--- src/test/java/ngdemo/rest/TestServer.java
+++ src/test/java/ngdemo/rest/TestServer.java
@@ -1,22 +1,13 @@
 package ngdemo.rest;
 
 import java.net.URI;
-import java.util.EnumSet;
-
-import javax.servlet.DispatcherType;
 
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.servlet.ServletHolder;
 import org.eclipse.jetty.webapp.WebAppContext;
-import org.glassfish.jersey.servlet.ServletContainer;
-
-import com.google.inject.servlet.GuiceFilter;
 
 final class TestServer {
 
     private static final int PORT = 8080;
-
-    private static final String PATH = "/api/*";
 
     private Server server;
 
@@ -24,20 +15,10 @@ final class TestServer {
         server = new Server(PORT);
 
         WebAppContext context = new WebAppContext();
-        context.setServer(server);
-
-        context.addFilter(GuiceFilter.class, PATH, EnumSet.allOf(DispatcherType.class));
-
-        ServletHolder holder = context.addServlet(ServletContainer.class, PATH);
-        holder.setInitParameter("javax.ws.rs.Application", "ngdemo.app.Application");
-
-        context.addServlet(holder, PATH);
-
         context.setContextPath("/");
         context.setWar("src/main/webapp");
 
         server.setHandler(context);
-
         server.start();
     }
 
